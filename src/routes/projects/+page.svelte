6fe25a63948f299719bf40cc7e1158468a352ae4
yousegit <ysaleh0207@gmail.com>
2024-06-26 16:21:08 +0200

    Coming Soon Commit

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,3 +3,27 @@
         <h1>Coming soon...</h1>
     </div>
 </section>
+
+<style>
+  #projects {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 35vh;
+  }
+
+  .projects-container {
+    text-align: center;
+  }
+
+  h1 {
+    color: grey;
+    font-size: 3em; /* Adjust this size as needed */
+  }
+
+  @media (max-width: 600px) {
+    h1 {
+      font-size: 2em; /* Adjust the size for mobile devices */
+    }
+  }
+</style>
